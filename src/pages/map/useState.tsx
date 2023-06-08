66de0d527f4a09f7c0a009d9a810f76dfc990ca6
Lucas Antunes de Freitas <lucas.freitas.ext@brf.com>
2023-06-07 23:32:28 -0300

    adjusts

--- src/pages/map/useState.tsx
+++ src/pages/map/useState.tsx
@@ -31,19 +31,19 @@ export const useMap = (props: Props) => {
     setLoading(false);
   };
 
-  const handleAccidents = () => {
-    while (date < endDate) {
-      const newDate = new Date(date);
-      newDate.setDate(newDate.getDate() + 1);
-      let result = accidentsData.filter(
-        (item) => item.getDatetime() <= newDate,
-      );
-      if (result.length > 0) {
-        setAccidents(result);
-      }
-      setDate(newDate);
-    }
-  };
+  // const handleAccidents = () => {
+  //   while (date < endDate) {
+  //     const newDate = new Date(date);
+  //     newDate.setDate(newDate.getDate() + 1);
+  //     let result = accidentsData.filter(
+  //       (item) => item.getDatetime() <= newDate,
+  //     );
+  //     if (result.length > 0) {
+  //       setAccidents(result);
+  //     }
+  //     setDate(newDate);
+  //   }
+  // };
 
   return { accidents, loading };
 };
